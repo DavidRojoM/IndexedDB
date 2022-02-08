955f3482a0679da679eeb60624621f432ef71bb3
David Rojo <davidrojo1997@gmail.com>
2022-02-08 23:18:13 +0100

    feat(domElements): add domElements

---
+++ js-no-dexie/js/dom.elements.js
@@ -0,0 +1,9 @@
+const domElements = {
+  $form: document.getElementById("form"),
+  $addBookForm: document.getElementById("addBookForm"),
+  $addAuthorForm: document.getElementById("addAuthorForm"),
+  $showAuthors: document.getElementById("showAuthors"),
+  $showBooks: document.getElementById("showBooks"),
+  $authorsList: document.getElementById("authorsList"),
+  $booksList: document.getElementById("booksList"),
+};
