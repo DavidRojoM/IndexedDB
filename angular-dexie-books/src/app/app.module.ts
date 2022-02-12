6e604d6c71f0b69013d0d7ab525b9078c11042d5
David Rojo <davidrojo1997@gmail.com>
2022-02-12 23:52:43 +0100

    fear(angular-1): add HomeComponent

--- angular-dexie-books/src/app/app.module.ts
+++ angular-dexie-books/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './core/domain/pages/home/home.component';
+import { FormComponent } from './core/shared/components/form/form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FormComponent, HomeComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
