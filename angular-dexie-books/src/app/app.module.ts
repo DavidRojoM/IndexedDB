65e9ff6aeb8213d617a16b87b2de5cdf51c46ade
David Rojo <davidrojo1997@gmail.com>
2022-02-13 23:52:19 +0100

    feat(angular-1): import components

--- angular-dexie-books/src/app/app.module.ts
+++ angular-dexie-books/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './core/domain/pages/home/home.component';
 import { FormComponent } from './core/shared/components/form/form.component';
+import { ListComponent } from './core/shared/components/list/list.component';
+import { FinderComponent } from './core/shared/components/finder/finder.component';
 
 @NgModule({
-  declarations: [AppComponent, FormComponent, HomeComponent],
+  declarations: [AppComponent, FormComponent, HomeComponent, ListComponent, FinderComponent],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
